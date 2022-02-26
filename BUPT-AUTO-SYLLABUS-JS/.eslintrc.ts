425c24993df4d64c226d50b9eb773882cdae63fe
3209571656 <3209571656@qq.com>
2022-02-27 01:27:40 +0800

    add: BAS-JS

---
+++ BUPT-AUTO-SYLLABUS-JS/.eslintrc.ts
@@ -0,0 +1,12 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+};
---
+++ BUPT-AUTO-SYLLABUS-JS/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>hello 丫丫</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
